167c937165d136f223075d8188ccdffc24db4810
MelvinCourant <melvin.courant1@gmail.com>
2024-03-07 11:50:53 +0100

    :technologist: delete user if member get an error at registration

--- back/app/controllers/auth_controller.ts
+++ back/app/controllers/auth_controller.ts
@@ -4,23 +4,26 @@ import {
 } from "#validators/register_user";
 import User from "#models/user";
 import Member from "#models/member";
-import db from '@adonisjs/lucid/services/db'
 
 export default class AuthController {
   public async register({ request, response }: HttpContext) {
     const payload = await request.validateUsing(createUserValidator)
-    await User.create({
+    const user = await User.create({
       email: payload.email,
       password: payload.password,
       role: 'leader', // TODO: will change after member connection is implemented
       image: payload.image,
     })
 
-    const user = await db.from('users').select('id').where('email', payload.email).firstOrFail()
     await Member.create({
       pseudo: payload.pseudo,
-      user_id: user.id,
+      user_id: user.id
     })
+      .catch((error) => {
+        user.delete()
+        throw error
+      })
+
     return response.status(201).created({ message: 'User created' })
   }
 
