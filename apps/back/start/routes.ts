c6af83e09dcc3080c56142a4f8d6535c6298adaf
MelvinCourant <melvin.courant1@gmail.com>
2024-03-17 21:41:53 +0100

    :sparkles: add request to get members of a guild

--- apps/back/start/routes.ts
+++ apps/back/start/routes.ts
@@ -9,8 +9,8 @@
 
 import router from '@adonisjs/core/services/router'
 import AuthController from '#controllers/auth_controller'
+import MembersController from "#controllers/members_controller";
 import app from '@adonisjs/core/services/app'
-import { middleware } from '#start/kernel'
 
 router.group(() => {
   const authController = new AuthController()
@@ -33,6 +33,10 @@ router.get('/uploads/:filename', async ({ params, response }) => {
   response.download(app.makePath('uploads', filename))
 })
 
-router.get('/', async () => {
-  return { hello: 'world' }
-}).use(middleware.auth())
+router.group(() => {
+  const membersController = new MembersController()
+
+  router.get('/members', async (data) => {
+    return membersController.index(data)
+  })
+}).prefix('api')
