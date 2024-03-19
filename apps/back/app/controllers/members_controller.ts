e14c3efd4ae670ec051b7da3f135a68893d9f6b4
Melvin <melvin.courant@lasuiteandco.com>
2024-03-19 17:41:51 +0100

    :sparkles: add request to exclude a member and fix condition to verify if user is the leader of the guild he want to get

--- apps/back/app/controllers/members_controller.ts
+++ apps/back/app/controllers/members_controller.ts
@@ -5,20 +5,22 @@ import Guild from "#models/guild";
 export default class MembersController {
   public async update({ auth, params, request, response }: HttpContext) {
     const user = await auth.authenticate()
-    const leadGuildId = await Guild
+    const leadGuild = await Guild
       .query()
       .where('leader_id', user.id)
       .select('id')
       .firstOrFail()
+    const leadGuildId = leadGuild.id
     const member = await Member
       .query()
       .where('id', params.id)
       .firstOrFail()
+    const memberGuildId = member.guild_id
 
     if(
       user.role !== 'admin' &&
       user.role !== 'leader' ||
-      leadGuildId.id !== member.guild_id
+      leadGuildId !== memberGuildId
     ) {
       return response.status(403).json({ error: 'Vous n\'avez pas les droits' })
     }
@@ -35,9 +37,39 @@ export default class MembersController {
     } else if(grade === member.grade) {
       return response.status(400).json({ error: `Le membre ${member.pseudo} a déjà le rôle ${grade}` })
     } else {
+      // TODO: managing the situation when the leader wants to hand over his role to another member
+      // TODO: managing the situation when the member have a account to update his role
       member.grade = grade
       await member.save()
       return response.json({ message: `Le rôle ${grade} a bien été attribué à ${member.pseudo}` })
     }
+  }
+
+  public async destroy({ auth, params, response }: HttpContext) {
+    const user = await auth.authenticate()
+    const leadGuild = await Guild
+      .query()
+      .where('leader_id', user.id)
+      .select('id')
+      .firstOrFail()
+    const leadGuildId = leadGuild.id
+    const member = await Member
+      .query()
+      .where('id', params.id)
+      .firstOrFail()
+    const memberGuildId = member.guild_id
+
+    if(
+      user.role !== 'admin' &&
+      user.role !== 'leader' ||
+      leadGuildId !== memberGuildId
+    ) {
+      return response.status(403).json({ error: 'Vous n\'avez pas les droits' })
+    }
+
+    // @ts-ignore
+    member.guild_id = null
+    await member.save()
+    return response.json({ message: `Le membre ${member.pseudo} a bien été exclu de la guilde` })
   }
 }
