2c5070f79fdbea5325361a70a1feb3928746e402
Melvin <melvin.courant@lasuiteandco.com>
2024-04-05 10:56:35 +0200

    :art: divide table component to table, table head and table rows

--- apps/front/src/components/tables/Table.vue
+++ apps/front/src/components/tables/Table.vue
@@ -1,104 +1,12 @@
 <script setup lang="ts">
 import '../../assets/css/components/tables/_table.scss'
-import {inject, ref, watch} from "vue";
-import Badge from "../utils/Badge.vue";
-import Grade from "../utils/Grade.vue";
-
-const columns = inject("columns");
-const data = ref(inject("data"));
-const rows = ref([]);
-const badges = ref([]);
-const env = import.meta.env;
-
-watch(data, () => {
-  rows.value = data.value.rows;
-  badges.value = data.value.badges;
-});
-
-function assetPlaceholderSrc() {
-  return new URL('../../assets/imgs/placeholder.jpg', import.meta.url).href;
-}
-
+import TableHead from "./TableHead.vue";
+import TableRows from "./TableRows.vue";
 </script>
 
 <template>
   <table class="table">
-    <thead>
-      <tr>
-        <th
-          v-for="column in columns"
-          :key="column"
-          :class="column.class"
-        >
-          {{ column.name }}
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr
-          v-for="(row, index) in rows"
-          :key="index"
-      >
-        <td
-            v-for="(info, key, index) in row"
-            :key="key"
-            :class="columns[index].class"
-        >
-          <ul
-            v-if="badges.includes(key)"
-          >
-            <template
-              v-for="(monster, index) in info"
-            >
-              <Badge
-                v-if="index < 7"
-                :key="monster.name"
-                :name="monster.name"
-                :element="monster.element"
-              />
-            </template>
-            <Badge
-              v-if="info.length > 7"
-              :key="'others'"
-              :name="'+' + (info.length - 7) + ' autres'"
-              element="dark-light"
-            />
-          </ul>
-          <img
-            v-else-if="
-              key === 'image' &&
-              info !== 'placeholder.jpg'
-            "
-            :src="`${env.VITE_URL}/uploads/${info}`"
-            alt="avatar"
-          />
-          <img
-              v-else-if="
-                key === 'image' &&
-                info === 'placeholder.jpg'
-              "
-              :src="assetPlaceholderSrc()"
-              alt="avatar"
-          />
-          <div
-            v-else-if="key === 'grade'"
-            class="members__grade-name"
-          >
-            <Grade
-              v-if="info !== 'member'"
-              :grade="info"
-            />
-            <span
-              v-else
-              class="members__grade--member"
-            >
-              💩
-            </span>
-            <span>{{ info }}</span>
-          </div>
-          <span v-else>{{ info }}</span>
-        </td>
-      </tr>
-    </tbody>
+    <TableHead />
+    <TableRows />
   </table>
 </template>
---
+++ apps/front/src/components/tables/TableHead.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import '../../assets/css/components/tables/_table-head.scss'
+import {inject} from "vue";
+
+const columns = inject("columns");
+</script>
+
+<template>
+  <thead class="table-head">
+    <tr>
+      <th
+        v-for="column in columns"
+        :key="column"
+        :class="column.class"
+      >
+        {{ column.name }}
+      </th>
+    </tr>
+  </thead>
+</template>
