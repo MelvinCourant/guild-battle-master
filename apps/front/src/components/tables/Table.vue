d52c71adf6ba520cbac294eabe6b096287c48813
MelvinCourant <melvin.courant1@gmail.com>
2024-04-07 19:03:32 +0200

    :lipstick: add loader to table

--- apps/front/src/components/tables/Table.vue
+++ apps/front/src/components/tables/Table.vue
@@ -2,13 +2,23 @@
 import '../../assets/css/components/tables/_table.scss'
 import TableHead from "./TableHead.vue";
 import TableRows from "./TableRows.vue";
+import Loader from "../utils/Loader.vue";
+import {inject, ref, watch} from "vue";
 
 defineEmits(['sort', 'actionSelected']);
+
+const data = ref(inject('data'))
+const rows = ref([])
+
+watch(data, () => {
+  rows.value = data.value.rows
+})
 </script>
 
 <template>
   <table class="table">
     <TableHead @sort="$emit('sort', $event)"/>
     <TableRows @actionSelected="$emit('actionSelected', $event)"/>
+    <Loader v-if="rows.length === 0"/>
   </table>
 </template>
---
+++ apps/front/src/components/utils/Loader.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import '../../assets/css/components/utils/_loader.scss'
+</script>
+
+<template>
+  <span class="loader"></span>
+</template>
