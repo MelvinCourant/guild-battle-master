0a8dcc8dace4a5cc8415a4e44c5fc80abe1c302c
MelvinCourant <melvin.courant1@gmail.com>
2024-04-08 14:15:23 +0200

    :adhesive_bandage: fix tooltip cropped in grid mode

--- apps/front/src/components/grids/Grid.vue
+++ apps/front/src/components/grids/Grid.vue
@@ -4,6 +4,8 @@ import {inject, ref, watch} from "vue";
 import GridCard from "./GridCard.vue";
 import Loader from "../utils/Loader.vue";
 
+defineEmits(['actionSelected'])
+
 const data = ref(inject('data'))
 const columns = inject('columns')
 const rows = ref([])
@@ -31,6 +33,7 @@ watch(data, () => {
           :columns="columns"
           :content="row"
           :actions="data.actions"
+          @actionSelected="$emit('actionSelected', $event)"
       />
     </ul>
     <Loader v-if="loading"/>
--- apps/front/src/components/grids/GridCard.vue
+++ apps/front/src/components/grids/GridCard.vue
@@ -25,6 +25,8 @@ defineProps(
   }
 )
 
+defineEmits(['actionSelected'])
+
 const env = import.meta.env;
 
 function placeholderSrc() {
