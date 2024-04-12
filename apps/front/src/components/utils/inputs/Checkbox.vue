3deb30b21483ca17c8d2580074e3f6bbe3803bf8
MelvinCourant <melvin.courant1@gmail.com>
2024-04-12 18:23:01 +0200

    :lipstick: style to checkbox

--- apps/front/src/components/utils/inputs/Checkbox.vue
+++ apps/front/src/components/utils/inputs/Checkbox.vue
@@ -1,4 +1,5 @@
 <script setup>
+import '../../../assets/css/components/utils/inputs/_checkbox.scss';
 defineProps({
   label: {
     type: String,
@@ -14,14 +15,23 @@ defineEmits(['sendValue']);
 </script>
 
 <template>
-  <div class="checkbox">
+  <label class="checkbox" :for="attributes.name">
     <input
-      :id="attributes.name"
-      :name="attributes.name"
-      :type="attributes.type"
-      :checked="attributes.checked"
-      @change="$emit('sendValue', attributes.name, $event.target.checked)"
+        :id="attributes.name"
+        :name="attributes.name"
+        :type="attributes.type"
+        :checked="attributes.checked"
+        :disabled="attributes.disabled"
+        class="checkbox__input"
+        @change="$emit('sendValue', attributes.name, $event.target.checked)"
     />
-    <label :for="attributes.name">{{ label }}</label>
-  </div>
+    <span class="checkbox__checkmark">
+      <svg xmlns="http://www.w3.org/2000/svg" width="14" height="12" viewBox="0 0 14 12" fill="none">
+        <path d="M4.59625 8.90625L1.46875 5.77875L0.403748 6.83625L4.59625 11.0288L13.5962 2.02875L12.5387 0.971252L4.59625 8.90625Z" fill="var(--white)"/>
+      </svg>
+    </span>
+    <span class="checkbox__label">
+      {{ label }}
+    </span>
+  </label>
 </template>
