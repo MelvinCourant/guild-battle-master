81584fa0b487e597826eba0b8b0d24ee45d11288
Melvin <melvin.courant@lasuiteandco.com>
2024-04-19 14:18:54 +0200

    :lipstick: add composition page

---
+++ apps/front/src/components/SearchCompositions.vue
@@ -0,0 +1,16 @@
+<script setup>
+import '../assets/css/components/_search-compositions.scss';
+import FiltersBar from "./utils/FiltersBar.vue";
+import Compositions from "./utils/Compositions.vue";
+
+defineEmits(['search']);
+</script>
+
+<template>
+  <div class="search-compositions">
+    <FiltersBar
+      @search="(inputName, value) => $emit('search', inputName, value)"
+    />
+    <Compositions/>
+  </div>
+</template>
