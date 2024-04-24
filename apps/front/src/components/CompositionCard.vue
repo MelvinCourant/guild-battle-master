15402f6e4a746208987444e8ef249269d5eabf13
Melvin <melvin.courant@lasuiteandco.com>
2024-04-24 13:48:15 +0200

    :lipstick: add style to composition card

--- apps/front/src/components/CompositionCard.vue
+++ apps/front/src/components/CompositionCard.vue
@@ -1,4 +1,5 @@
 <script setup>
+import '../assets/css/components/_composition-card.scss';
 import More from "./utils/More.vue";
 import Defense from "./utils/Defense.vue";
 
@@ -17,19 +18,21 @@ defineProps({
 <template>
   <div class="composition-card">
     <div class="composition-card__header">
+      <div class="composition-card__actions-container">
+        <div class="composition-card__actions">
+          <More
+            :actions="actions"
+          />
+        </div>
+      </div>
       <p class="composition-card__grade">Tour {{ composition.grade }} nat</p>
       <h3 class="composition-card__title">{{ composition.name }}</h3>
-      <More
-          :actions="actions"
-      />
     </div>
     <div class="composition-card__defenses">
       <Defense
-          v-for="defense in composition.defenses"
-          :key="defense.id"
-          :leader="defense.leader"
-          :second="defense.second"
-          :third="defense.third"
+          :leader="composition.defenses[0].leader"
+          :second="composition.defenses[0].second"
+          :third="composition.defenses[0].third"
       />
     </div>
   </div>
