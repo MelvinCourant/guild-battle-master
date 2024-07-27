c552c8e11c2a05f2d6e4228099492dee850b3b14
MelvinCourant <melvin.courant1@gmail.com>
2024-07-27 11:48:05 +0200

    :sparkles: add pagination to list guilds and list users

--- apps/back/app/controllers/admin_controller.ts
+++ apps/back/app/controllers/admin_controller.ts
@@ -4,35 +4,60 @@ import Member from '#models/member'
 import User from '#models/user'
 
 export default class AdminController {
-  async listGuilds({ i18n, auth, response }: HttpContext) {
+  async listGuilds({ i18n, auth, request, response }: HttpContext) {
     const user = await auth.authenticate()
 
     if (user.role !== 'admin') {
       return response.status(403).json({ message: i18n.t('messages.forbidden') })
     }
 
-    const guilds = await Guild.all()
-    let guildsData = []
-
-    for (let guild of guilds) {
-      const leader = await Member.query().where('user_id', guild.leader_id).firstOrFail()
-
-      guildsData.push({
-        guild: {
-          id: guild.id,
-          guild_id_json: guild.guild_id_json,
-          name: guild.name,
-          image: guild.image,
-          created_at: guild.createdAt,
-        },
-        leader: {
-          user_id: leader.user_id,
-          pseudo: leader.pseudo,
-        },
-      })
+    const page = request.input('page') || 1
+    const pageSize = request.input('pageSize') || 25
+    const guilds = await Guild.query()
+      .select('id', 'guild_id_json', 'name', 'image', 'leader_id', 'created_at')
+      .paginate(page, pageSize)
+    guilds.namingStrategy = {
+      paginationMetaKeys() {
+        return {
+          total: 'total',
+          perPage: 'per_page',
+          currentPage: 'current_page',
+          lastPage: 'last_page',
+          firstPage: 'first_page',
+          firstPageUrl: 'first_page_url',
+          lastPageUrl: 'last_page_url',
+          nextPageUrl: 'next_page_url',
+          previousPageUrl: 'previous_page_url',
+        }
+      },
     }
 
-    return response.json(guildsData)
+    const guildsData = await Promise.all(
+      guilds.toJSON().data.map(async (guild) => {
+        const leader = await Member.query()
+          .where('user_id', guild.leader_id)
+          .select('pseudo')
+          .firstOrFail()
+        return {
+          guild: {
+            id: guild.id,
+            guild_id_json: guild.guild_id_json,
+            name: guild.name,
+            image: guild.image,
+            created_at: guild.createdAt,
+          },
+          leader: {
+            user_id: guild.leader_id,
+            pseudo: leader.pseudo,
+          },
+        }
+      })
+    )
+
+    return response.json({
+      meta: guilds.toJSON().meta,
+      data: guildsData,
+    })
   }
 
   async searchGuild({ i18n, auth, request, response }: HttpContext) {
@@ -65,6 +90,21 @@ export default class AdminController {
     const page = request.input('page') || 1
     const pageSize = request.input('pageSize') || 25
     const guilds = await query.paginate(page, pageSize)
+    guilds.namingStrategy = {
+      paginationMetaKeys() {
+        return {
+          total: 'total',
+          perPage: 'per_page',
+          currentPage: 'current_page',
+          lastPage: 'last_page',
+          firstPage: 'first_page',
+          firstPageUrl: 'first_page_url',
+          lastPageUrl: 'last_page_url',
+          nextPageUrl: 'next_page_url',
+          previousPageUrl: 'previous_page_url',
+        }
+      },
+    }
 
     const guildsData = await Promise.all(
       guilds.toJSON().data.map(async (guild) => {
@@ -94,56 +134,75 @@ export default class AdminController {
     })
   }
 
-  async listUsers({ i18n, auth, response }: HttpContext) {
+  async listUsers({ i18n, auth, request, response }: HttpContext) {
     const user = await auth.authenticate()
 
     if (user.role !== 'admin') {
       return response.status(403).json({ message: i18n.t('messages.forbidden') })
     }
 
-    const users = await User.query().where('role', '!=', 'admin')
-    let usersData = []
-
-    for (let user of users) {
-      const userInfos = {
-        id: user.id,
-        username: user.username,
-        image: user.image,
-        created_at: user.createdAt,
-      }
-      const member = await Member.query()
-        .where('user_id', user.id)
-        .select('id', 'pseudo', 'guild_id')
-        .first()
-
-      if (!member) {
-        usersData.push({
-          user: userInfos,
-          member: null,
-        })
-        continue
-      }
-
-      const guild = await Guild.query().where('id', member.guild_id).firstOrFail()
-
-      usersData.push({
-        user: {
-          id: user.id,
-          username: user.username,
-          image: user.image,
-          created_at: user.createdAt,
-        },
-        member: {
-          id: member.id,
-          pseudo: member.pseudo,
-        },
-        guild: {
-          id: guild.id,
-          name: guild.name,
-        },
-      })
+    const page = request.input('page') || 1
+    const pageSize = request.input('pageSize') || 25
+    const users = await User.query().where('role', '!=', 'admin').paginate(page, pageSize)
+    users.namingStrategy = {
+      paginationMetaKeys() {
+        return {
+          total: 'total',
+          perPage: 'per_page',
+          currentPage: 'current_page',
+          lastPage: 'last_page',
+          firstPage: 'first_page',
+          firstPageUrl: 'first_page_url',
+          lastPageUrl: 'last_page_url',
+          nextPageUrl: 'next_page_url',
+          previousPageUrl: 'previous_page_url',
+        }
+      },
     }
 
-    return response.json(usersData)
+    const usersData = await Promise.all(
+      users.toJSON().data.map(async (user) => {
+        const member = await Member.query()
+          .where('user_id', user.id)
+          .select('id', 'pseudo', 'guild_id')
+          .first()
+
+        if (!member) {
+          return {
+            user: {
+              id: user.id,
+              username: user.username,
+              image: user.image,
+              created_at: user.createdAt,
+            },
+            member: null,
+          }
+        }
+
+        const guild = await Guild.query().where('id', member.guild_id).firstOrFail()
+
+        return {
+          user: {
+            id: user.id,
+            username: user.username,
+            image: user.image,
+            created_at: user.createdAt,
+          },
+          member: {
+            id: member.id,
+            pseudo: member.pseudo,
+          },
+          guild: {
+            id: guild.id,
+            name: guild.name,
+          },
+        }
+      })
+    )
+
+    return response.json({
+      meta: users.toJSON().meta,
+      data: usersData,
+    })
   }
 }
